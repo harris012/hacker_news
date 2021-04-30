2c1b43643b82c25909212589b6546901c95bd715
harris012 <harissaeed39@yahoo.com>
2021-04-30 11:27:22 +0200

    Updates

---
+++ precache-manifest.f59254c27f8c6e6f4ea1cfe0b0cedfb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690a0efd8f3eca92e6832179670387bf",
+    "url": "/hacker_news/index.html"
+  },
+  {
+    "revision": "8cd2741fa8a55e9bdcd8",
+    "url": "/hacker_news/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/hacker_news/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker_news/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd2741fa8a55e9bdcd8",
+    "url": "/hacker_news/static/js/main.1a9acd67.chunk.js"
+  },
+  {
+    "revision": "f0a2c76004705a04a5b9",
+    "url": "/hacker_news/static/js/runtime-main.c3a6576b.js"
+  }
+]);
